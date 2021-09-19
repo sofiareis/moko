f8943ea40ebe5fae28c293875de7616cda710895
emilyl <emily.lukas@gmail.com>
2021-09-18 21:51:01 -0700

    fixed store and cart stuff

--- backend/modules/storeModule.js
+++ backend/modules/storeModule.js
@@ -37,4 +37,17 @@ Store.getAll = (result) => {
   });
 }
 
+Store.findById = (result) => {
+  sql.query("SELECT * FROM STORE WHERE userID=\"' + userID + '\"", (err, res) => {
+      if (err) {
+          console.log("error: ", err);
+          result(err, null);
+          return;
+      }
+
+      console.log("stores: ", res);
+      result(null, res);
+  });
+}
+
 module.exports = Store;
--- frontend/moko/navigation/StoreStack.js
+++ frontend/moko/navigation/StoreStack.js
@@ -2,10 +2,11 @@ import React, { useState } from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 import Ionicons from 'react-native-vector-icons/Ionicons';
-import { createStackNavigator } from '@react-navigation/stack'; 
+import { createStackNavigator } from '@react-navigation/stack';
 
 import Store from '../screens/Store.js';
 import StoreItem from '../screens/StoreItem.js';
+import EditCreateStoreItem from '../screens/EditCreateStoreItem.js';
 
 const Stack = createStackNavigator();
 
@@ -17,6 +18,7 @@ function StoreStack({ navigation }) {
                       }}>
     <Stack.Screen name="Store" component={Store} />
     <Stack.Screen name="StoreItem" component={StoreItem} />
+    <Stack.Screen name="EditCreateStoreItem" component={EditCreateStoreItem} />
     </Stack.Navigator>
   );
 }
