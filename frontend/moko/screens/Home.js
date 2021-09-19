3783860c8f74c128214fe1bc001b9752356a6e98
veronicabcd <n.veronica0618@gmail.com>
2021-09-19 00:55:28 -0700

    storeFront edits

--- frontend/moko/screens/Home.js
+++ frontend/moko/screens/Home.js
@@ -113,7 +113,7 @@ function HomeScreen({ navigation }) {
           extraData={listItems}
           keyExtractor={item => item.description}
           renderItem={({item}) => (
-              <TouchableOpacity onPress={() => navigation.navigate('StoreFront', { paramKey: item.iD })}>
+              <TouchableOpacity onPress={() => navigation.navigate('StoreFront', { storeName: item.name, desc: item.description })}>
                 <View style = {styles.vendorRectangle}>
                     <Text style = {styles.vendorName}>{item.name}</Text>
                     <View style = {{flexDirection: 'row', marginTop: 10}}>
