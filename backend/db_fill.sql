f4c5f15db320b4565cd55ec01aeba045dbb9b3ed
emilyl <emily.lukas@gmail.com>
2021-09-18 23:32:35 -0700

    modified db scripts

---
+++ backend/db_fill.sql
@@ -0,0 +1,4 @@
+INSERT INTO CART_ITEM(userID, storeItemID, quantity) VALUES(1, 1, 1);
+INSERT INTO CART_ITEM(userID, storeItemID, quantity) VALUES(1, 2, 2);
+INSERT INTO CART_ITEM(userID, storeItemID, quantity) VALUES(1, 3, 1);
+INSERT INTO CART_ITEM(userID, storeItemID, quantity) VALUES(1, 4, 1);
